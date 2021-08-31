0f3e413987e70a4b2e515ec31ecad178f25aa866
Remy-Desire <airdesire38@gmail.com>
2021-08-31 15:51:15 +0200

    fonction icone dynamique & temperature par jour

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,9 @@ const temperature = document.querySelector('.temperature');
 const localisation = document.querySelector('.localisation');
 const heure = document.querySelectorAll('.heure-nom-prevision');
 const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
+const joursDiv = document.querySelectorAll('.jour-nom-prevision');
+const tempJourDiv = document.querySelectorAll('.jour-prevision-valeur');
+const imgIcone = document.querySelector('.logo-meteo')
 
 //Pour récupérer la position de l'utilisateur
 if(navigator.geolocation) {
@@ -58,6 +61,24 @@ function AppelAPI(lon, lat) {
         //temp pour 3h 
         for(let j = 0; j < tempPourH.length; j++) {
             tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)} °`
+        }
+
+        //trois premieres lettres des jours 
+
+        for(let k = 0; k < tabJoursEnOrdre.length; k++) {
+            joursDiv[k].innerText = tabJoursEnOrdre[k].slice(0, 3);
+        }
+
+        // Temp par Jour 
+        for(let m = 0; m < 7; m++) {
+            tempJourDiv[m].innerText = `${Math.trunc(resultatsAPI.daily[m + 1].temp.day)}°`
+        }
+
+        // Icone dynamique 
+        if(heureActuelle >= 6 && heureActuelle < 21) {
+            imgIcone.src = `../ressources/jour/${resultatsAPI.current.weather[0].icon}.svg`;
+        } else {
+            imgIcone.src = `../ressources/nuit/${resultatsAPI.current.weather[0].icon}.svg`;
         }
 
     })
